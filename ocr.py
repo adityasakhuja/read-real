89217abd32f83d96c11c962c02b6a402c57c9a6d
Aditya Sakhuja <aditya.sakhuja@live.com>
2016-06-20 20:25:46 +0100

    program works

--- ocr.py
+++ ocr.py
@@ -1,11 +1,11 @@
-##########################
-## Name: ocr.py
-## Author: Aditya Sakhuja
-## Date: Monday, 20 June 2016
-## Description: Performs Optical Character Recognition
-## using the pytesseract library - a Python wrapper for 
-## Google's Tesseract-OCR.
-##########################
+##########################################################
+## Name: ocr.py						##
+## Author: Aditya Sakhuja				##
+## Date: Monday, 20 June 2016				##
+## Description: Performs Optical Character Recognition	##
+## using the pytesseract library - a Python wrapper for ##
+## Google's Tesseract-OCR.				##
+##########################################################
 
 
 try:
@@ -16,10 +16,8 @@ import pytesseract
 
 #print(pytesseract.image_to_string(Image.open('TestImages/test4.jpg')))
 
-
+## Function used by main function to initiate OCR
 def read():
 	characters = pytesseract.image_to_string(Image.open('TestImages/test.jpg'))
 	return characters
-
-print read()
 
--- read-real.py
+++ read-real.py
@@ -1,22 +1,24 @@
+#############################
 ## Name: read-real.py
 ## Author: Aditya Sakhuja
 ## Date: Monday, 20 June 2016
 ## Description: 
 ##
-
-
 #############################
+
+
 import sys
 from time import sleep
-from picamera import PiCamera
-try:
-	import Image
-except ImportError:
-	from PIL import Image
-import pytesseract
 
+## For Image Capture
+from picamera import PiCamera
+
+## For Optical Character Recognition
 from ocr import read
-############################
+
+## For Translation
+from translation import translate
+
 
 
 ## Camera Setup and Capture
@@ -26,7 +28,10 @@ sleep(10)
 camera.capture('image.jpg')
 
 ## Optical Character Recognition
-read()
+text = read()
+for letter in text:
+	braille = translate(letter)
+	print letter + braille
 
 ## Translation
 
